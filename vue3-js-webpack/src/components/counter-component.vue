1fb3d74819200cb8a185d2ef3144fe5540b86512
Kazuyoshi Tomita <kztomita@gmail.com>
2023-02-25 14:37:25 +0900

    JavaScript + Webpackを使った例

---
+++ vue3-js-webpack/src/components/counter-component.vue
@@ -0,0 +1,25 @@
+<style scoped>
+button {
+  color: black;
+  border-radius: 2px;
+  border: 1px solid gray;
+  background: linear-gradient(to bottom, #fcfcfc, #ddd);
+  padding: 5px 10px;
+}
+button:hover {
+  background: linear-gradient(to bottom, #fcfcfc, #fcfcfc);
+}
+button:active {
+  background: linear-gradient(to bottom, #eee, #ccc);
+}
+
+</style>
+
+<template>
+  <div class="container">
+    <p>Counter: {{ counter }}</p>
+    <button v-on:click="counter++">Increment</button>
+  </div>
+</template>
+
+<script src="./counter-component.js"></script>
---
+++ vue3-js-webpack/webpack.config.js
@@ -0,0 +1,48 @@
+const { VueLoaderPlugin } = require('vue-loader');
+const webpack = require('webpack');
+
+module.exports = {
+  //mode: 'production',
+  mode: 'development',
+
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: __dirname + '/public'
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.vue$/,
+        loader: "vue-loader",
+      },
+      /* .vue内で<style>タグを使うのに必要 */
+      {
+        test: /\.css$/,
+        use: [
+          'vue-style-loader',
+          'css-loader'
+        ]
+      }
+    ],
+  },
+  resolve: {
+    alias: {
+      // runtime versionではなくテンプレートコンパイラを含む完全ビルドを使う
+      // https://jp.vuejs.org/v2/guide/installation.html
+      'vue$': 'vue/dist/vue.esm-bundler.js'
+    },
+    // importでの拡張子省略設定
+    extensions: [
+      '.js',
+    ],
+  },
+  plugins: [
+    new VueLoaderPlugin(),
+    new webpack.DefinePlugin({
+      __VUE_OPTIONS_API__: true,
+      __VUE_PROD_DEVTOOLS__: false
+    }),
+  ],
+};
